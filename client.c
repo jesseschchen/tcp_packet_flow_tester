336b5dca15e2f0fc9d39731ac85a58c99109253f
root <jesseschchen@gmail.com>
2021-02-24 18:43:28 -0800

    v1

---
+++ client.c
@@ -0,0 +1,81 @@
+#include <sys/socket.h> // socket()
+#include <unistd.h> 	// read(), write()
+#include <fcntl.h> 		// open(), close()
+#include <errno.h>		// errno
+#include <stdlib.h>		// malloc(), free()
+#include <netinet/in.h> // struct sockaddr_in
+#include <stdio.h> 		// sprintf()
+#include <arpa/inet.h>	// inet_addr()
+#include <string.h>		// strlen()
+
+
+
+// opens a connection to client-side connection to server_ip on port and returns a socket fd
+int open_tcp_connection(char* server_ip, int port) {
+	int socket_fd;
+	struct sockaddr_in address;
+
+	// opens a socket 
+	if ((socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("failed to create socket");
+		exit(1);
+	}
+	printf("created socket!\n");
+
+	// no need to bind because client doesn't care what port it's on
+
+	// initializes a sockaddr_in structure to connect to
+	address.sin_family = AF_INET; // IPv4
+	address.sin_addr.s_addr = inet_addr(server_ip); // converts ip from char* to binary
+	address.sin_port = htons(port); // converts from host to network byte order
+
+	// attempt to connect to <server_ip>:<port>
+	if (connect(socket_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+		fprintf(stderr, "failed to connect to %s:%i\n", server_ip, port);
+		exit(1);
+	}
+	printf("successfully connected to %s:%i\n", server_ip, port);
+
+	// returns socket file descriptor
+	return socket_fd;
+}
+
+
+// infinite loops and sends packets until killed
+int send_packets(int socket_fd, int buf_size) {
+	void* buf = malloc(buf_size);
+
+	size_t pack_id = 0;
+	printf("sizeof(size_t): %li\n", sizeof(pack_id));
+
+	for (; ; pack_id++) {
+		printf("packet_id: %li\n", pack_id);
+		memcpy(buf, &pack_id, sizeof(size_t));
+		if (send(socket_fd, buf, buf_size, 0) != buf_size) {
+			perror("send()");
+		}
+	}
+}
+
+// ./tcp_client <server_ip> <port> <filename>
+int main(int argc, char* argv[]) {
+	if (argc != 4){
+		fprintf(stderr, "usage: %s <server_ip_address> <port> <buf_size> \n", argv[0]);
+		exit(1);
+	}
+
+	char* server_ip = argv[1]; // server ip
+	int port = atoi(argv[2]);  // port number
+	int buf_size = atoi(argv[3]);  // filename to transfer
+
+	// opens connection to server_ip:port
+	int socket_fd = open_tcp_connection(server_ip, port);
+
+	// starts sending packets forever
+	send_packets(socket_fd, buf_size);
+
+
+
+	close(socket_fd);
+
+}
---
+++ server.c
@@ -0,0 +1,90 @@
+#include <sys/socket.h> // socket()
+#include <unistd.h> 	// read(), write()
+#include <fcntl.h> 		// open(), close()
+#include <errno.h>		// errno
+#include <stdlib.h>		// malloc(), free()
+#include <netinet/in.h> // struct sockaddr_in
+#include <stdio.h> 		// sprintf()
+#include <arpa/inet.h>	// inet_addr()
+#include <string.h> 	// memcpy(), strlen()
+
+#define BUF_SIZE 2048
+#define MAX_CLIENTS 20
+
+typedef struct file_info {
+	char* filename;
+	int size;
+} file_info;
+
+void recv_forever(int connection_fd, int buf_size) {
+	void* buf = malloc(buf_size);
+	while(recv(connection_fd, buf, buf_size, 0) > 0);
+}
+
+
+
+// starts the server to listen on port
+// receives connections until user hits ctrl-c
+void open_server(int port, int buf_size) {
+	int server_fd, connection_fd;
+	int recvd_file_size;
+	struct sockaddr_in address;
+	int addrlen = sizeof(address);
+	file_info received_file;
+
+
+	// opens a socket
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+		perror("failed to create socket");
+		exit(1);
+	}
+	printf("created socket!\n");
+
+	// initializes a sockaddr_in structure to listen on 
+	address.sin_family = AF_INET; // IPv4
+	address.sin_addr.s_addr = htonl(INADDR_ANY); // binds to all available interfaces
+	address.sin_port = htons(port); // converts from host to network byte order
+
+	// binds server_fd to a specific sockaddr_in
+	int enable = 1;
+	setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
+	if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+		fprintf(stderr, "unable to bind to port %i\n", port);
+		exit(1);
+	}
+	printf("bound socket to port %i\n", port);
+
+	// listen for new connections on the created/bound socket
+	// ** waits for TCP SYNs
+	if (listen(server_fd, MAX_CLIENTS) < 0) {
+		perror("listen failed");
+		exit(1);
+	}
+	printf("listening...\n");
+
+	// accepts 1 connection 
+	printf("\n");
+	// accept TCP connection
+	if ((connection_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+		fprintf(stderr, "failed to accept connection on port %i\n", port);
+		exit(1);
+	}
+	printf("accepted connection!\n");\
+	recv_forever(connection_fd, buf_size); // receives receives on this socket forever(theoretically)
+}
+
+
+
+// ./tcp_server <port> 
+int main(int argc, char* argv[]) {
+	if (argc != 3) {
+	 	fprintf(stderr, "usage: ./tcp_server <port> <buf_size>\n");
+	 	exit(1);
+	}
+
+
+	int port = atoi(argv[1]);
+	int buf_size = atoi(argv[2]);
+
+	open_server(port, buf_size);
+}
